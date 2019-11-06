b50f5a21baeea4e3d51351bffc32f7627314d96f
marcio heleno <marcio012@edu.unifor.br>
2019-11-06 09:36:33 -0300

    Updates

---
+++ precache-manifest.ee4b04c325f71ae17b8a18607200703f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a95973f12bf6de08f1c8b5055067aa3",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "06cb589c4d2a79fe591a",
+    "url": "/portifolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "15b3b174c655bd475204",
+    "url": "/portifolio/static/js/2.2b29d8f2.chunk.js"
+  },
+  {
+    "revision": "06cb589c4d2a79fe591a",
+    "url": "/portifolio/static/js/main.3f843ea6.chunk.js"
+  },
+  {
+    "revision": "6ef6d3b5aefb74650446",
+    "url": "/portifolio/static/js/runtime-main.07aeb986.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portifolio/static/media/logo.25bf045c.svg"
+  }
+]);
